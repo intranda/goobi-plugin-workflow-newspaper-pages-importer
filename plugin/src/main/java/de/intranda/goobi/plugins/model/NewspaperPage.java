0f3f8cde91252590460e81bf5480431c03741c2a
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2023-12-08 10:44:31 +0100

    some javadoc

--- plugin/src/main/java/de/intranda/goobi/plugins/model/NewspaperPage.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/model/NewspaperPage.java
@@ -1,3 +1,29 @@
+/**
+ * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
+ * 
+ * Visit the websites for more information.
+ *          - https://goobi.io
+ *          - https://www.intranda.com
+ *          - https://github.com/intranda/goobi-workflow
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Linking this library statically or dynamically with other modules is making a combined work based on this library. Thus, the terms and conditions
+ * of the GNU General Public License cover the whole combination. As a special exception, the copyright holders of this library give you permission to
+ * link this library with independent modules to produce an executable, regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of your choice, provided that you also meet, for each linked independent module, the terms and
+ * conditions of the license of that module. An independent module is a module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but you are not obliged to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ */
+
 package de.intranda.goobi.plugins.model;
 
 import java.nio.file.Path;
@@ -10,8 +36,13 @@ import org.apache.commons.lang3.StringUtils;
 
 import lombok.Getter;
 
+/**
+ * Represents a page of a newspaper in the digitization workflow. Provides methods for extracting and formatting date information from the file name,
+ * checking the validity of the file, and obtaining a formatted European-style date.
+ */
 @Getter
 public class NewspaperPage {
+
     private static final Pattern YEAR_PATTERN = Pattern.compile("2\\d{3}");
     private static final Pattern DATE_PATTERN = Pattern.compile("\\d{4}[\\W_]+\\d{2}[\\W_]+\\d{2}");
     private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d{3}");
@@ -27,6 +58,11 @@ public class NewspaperPage {
     private String day;
     private String pageNumber;
 
+    /**
+     * Constructs a NewspaperPage object with the given file path.
+     *
+     * @param filePath The path to the newspaper page file.
+     */
     public NewspaperPage(Path filePath) {
         this.filePath = filePath;
         fileName = filePath.getFileName().toString();
@@ -41,23 +77,49 @@ public class NewspaperPage {
         }
     }
 
+    /**
+     * Gets the European-style formatted date (dd.mm.yyyy) of the newspaper page.
+     *
+     * @return The formatted date.
+     */
     public String getDateEuropean() {
         return day + "." + month + "." + year;
     }
 
+    /**
+     * Extracts the date information from the given file name.
+     *
+     * @param fileName The file name from which to extract the date.
+     * @return The extracted date or an empty string if not found.
+     */
     public static String getDateFromFileName(String fileName) {
         Matcher matcher = DATE_PATTERN.matcher(fileName);
         return matcher.find() ? matcher.group() : "";
     }
 
+    /**
+     * Gets the fine-formatted date (dd. MMM yyyy) of the newspaper page.
+     *
+     * @return The formatted date.
+     */
     public String getDateFine() {
         return LocalDate.parse(date, DATE_TIME_PARSER).format(DATE_TIME_FORMATTER);
     }
 
+    /**
+     * Checks if the file size is valid (greater than 0 and readable).
+     *
+     * @return True if the file size is valid, false otherwise.
+     */
     public boolean isFileSizeValid() {
         return filePath.toFile().canRead() && filePath.toFile().length() > 0;
     }
 
+    /**
+     * Checks if the file is valid based on date, page number, and file size.
+     *
+     * @return True if the file is valid, false otherwise.
+     */
     public boolean isFileValid() {
         boolean vdate = isDateValid();
         boolean vpage = isPageNumberValid();
@@ -65,6 +127,11 @@ public class NewspaperPage {
         return vdate && vpage && vsize;
     }
 
+    /**
+     * Checks if the file is invalid based on date, page number, and file size.
+     *
+     * @return True if the file is invalid, false otherwise.
+     */
     public boolean isFileInvalid() {
         return !isFileValid();
     }
@@ -76,5 +143,4 @@ public class NewspaperPage {
     private boolean isPageNumberValid() {
         return pageNumber.length() == 3 && NUMBER_PATTERN.matcher(pageNumber).find();
     }
-
 }
